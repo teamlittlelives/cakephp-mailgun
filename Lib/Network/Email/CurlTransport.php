20de8c108d6e3b9537bdc0c7b19fb33d98fc9fb6
Nick <nicholas@littlelives.com>
2016-04-12 14:14:11 +0800

    allow rerouting for non production environment

--- Lib/Network/Email/CurlTransport.php
+++ Lib/Network/Email/CurlTransport.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * Mailgun curl class
+ * Forked by LittleLives https://github.com/teamlittlelives/cakephp-mailgun
  *
  * Enables sending of email over mailgun via curl
  *
@@ -24,7 +25,27 @@ class CurlTransport extends AbstractTransport {
  * @params CakeEmail $email
  * @return array
  */
-	public function send(CakeEmail $email) {
+
+    public function reroute_email($email)
+    {
+        $ll_domain = "littlelives.com";
+        $parts = explode("@", $email);
+
+        $localpart = $parts[0];
+        $domain = $parts[1];
+
+        if(substr($localpart,0,3) != "ll." && substr($localpart,0,4) != "bin.") {
+                $localpart = "ll.".$localpart.".".$domain;
+        } else {
+                $localpart = $localpart.".".$domain;
+        }
+
+        $reroute_email = $localpart."@".$ll_domain;
+
+        return $reroute_email;
+    }
+
+    public function send(CakeEmail $email) {
         $post = array();
         $post_preprocess = array_merge(
             $email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'bcc', 'subject')),
@@ -33,11 +54,34 @@ class CurlTransport extends AbstractTransport {
                 'html' => $email->message(CakeEmail::MESSAGE_HTML)
             )
         );
+
+        $email_add_params = array('To','Bcc','Cc');
+
         foreach ($post_preprocess as $k => $v) {
             if (! empty($v)) {
-                $post[strtolower($k)] = $v;
+                if(in_array($k,$email_add_params)) {
+                    $emails = explode(', ',$v);
+                    $email_arr = array();
+                    foreach($emails as $email_key => $email_address) {
+                        $routed_email = $this->reroute_email($email_address);
+                        array_push($email_arr, $routed_email);
+                    }
+                    $joined_emails = join($email_arr,', ');
+                    $post[strtolower($k)] = $joined_emails;
+                } else {
+                    $post[strtolower($k)] = $v;
+                }
             }
         }
+
+                if ($attachments = $email->attachments()) {
+                        $i = 1;
+                        foreach ($attachments as $attachment) {
+                            $post['attachment[' . $i . ']'] = "@" . $attachment["file"];
+                            $i++;
+                        }
+                    }
+
 
         $ch = curl_init('https://api.mailgun.net/v2/' . $this->_config['mailgun_domain'] . '/messages');
 
